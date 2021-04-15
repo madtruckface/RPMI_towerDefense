e8490f5b7385f29b1bc251a9c1372086e166b15b
madtruckface <veronikafaulds@gmail.com>
2021-04-15 11:39:51 +0200

    no message

---
+++ rpmi_towerDefense/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+    [Tooltip("Velocidad de la bala")]
+    public float bulletSpeed;
+
+    [Tooltip("Referencia al enemigo que voy a matar")]
+    public GameObject enemy;
+
+    void Start() {
+        // La bala se destruirá a los 3 segundos para evitar problemas de memoria
+        Destroy(gameObject, 3);
+    }
+
+    void Update() {
+        // Movimiento de la bala en el eje Y según la velocidad especificada
+        transform.position = Vector3.MoveTowards(transform.position, enemy.transform.position, bulletSpeed);
+        //transform.Translate(0, bulletSpeed, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        // La bala se destruye en cuanto colisiona con algún objeto
+        Destroy(gameObject);
+    }
+}
---
+++ rpmi_towerDefense/Assets/Scripts/TowerShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShoot : MonoBehaviour {
+    [Tooltip("Referencia al prefab de la bala")]
+    public GameObject bulletPrefab;
+
+    [Tooltip("Referencia al enemigo que voy a disparar")]
+    public GameObject enemy;
+
+    void Update() {
+
+        // Instancia una bala cada vez que el jugador pulsa el espacio
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject bala = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bala.GetComponent<BulletMovement>().enemy = enemy;
+        }
+    }
+}
